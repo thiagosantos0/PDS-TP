d7eb3d4e0c52ed7a8b3f06ae9d8e89111cf8ac3c
Thiago Santos <thiagosantosgg0@gmail.com>
2022-11-13 20:23:16 -0300

    Concluindo remoção de artigos

--- frontend/src/routes/DeleteArticle.js
+++ frontend/src/routes/DeleteArticle.js
@@ -1,4 +1,7 @@
+import { apiAxios } from '../app/apiAxios.js';
+
 export async function action({ params }) {
   console.log(`Delete article ${params.docId}`);
+  await apiAxios.delete(`article/delete-article/${params.docId}`); 
   return { ok: true };
 }
