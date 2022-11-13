b0b22c270be171b4b7b66e815afe9e70a3f43201
joaopmo <joao.oliveira@dcc.ufmg.br>
2022-11-13 16:41:44 -0300

    conexão api login route

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -7,12 +8,31 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
-import { Form, redirect } from 'react-router-dom';
+import { Form, useActionData, useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectCredentials,
+  setCredentials,
+} from '../features/auth/authSlice.js';
+
+import { apiAxios } from '../app/apiAxios.js';
 
 const MuiForm = styled(Form)({});
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const credentials = useSelector(selectCredentials);
+  const actionData = useActionData();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (actionData?.status === 200) {
+      dispatch(setCredentials(actionData.data.userInfo));
+    }
+    if (credentials.isLoggedIn) navigate(`/${credentials.id}/articles`);
+  }, [actionData, credentials, dispatch, navigate]);
+
   return (
     <Container component='main' maxWidth='xs'>
       <Box
@@ -39,6 +59,8 @@ const Login = () => {
             label='Email'
             autoComplete='email'
             autoFocus
+            error={actionData?.status === 404}
+            helperText={actionData?.status === 404 && 'Usuário não cadastrado'}
           />
           <TextField
             margin='normal'
@@ -49,6 +71,8 @@ const Login = () => {
             label='Senha'
             type='password'
             autoComplete='current-password'
+            error={actionData?.status === 401}
+            helperText={actionData?.status === 401 && 'Senha Incorreta'}
           />
           <Button
             type='submit'
@@ -73,9 +97,18 @@ const Login = () => {
 
 export async function action({ request }) {
   const formData = await request.formData();
-  const updates = Object.fromEntries(formData);
-  console.log(updates);
-  return redirect('/userId/articles');
+  const authData = Object.fromEntries(formData);
+  let data = {};
+  try {
+    const response = await apiAxios.post('/auth/login', authData);
+    data.data = response.data;
+    data.status = response.status;
+  } catch (e) {
+    data.error = e.response.data;
+    data.status = e.response.status;
+  }
+
+  return data;
 }
 
 export default Login;
