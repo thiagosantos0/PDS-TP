6be04b221db5d0df874596e37d1f48faecca0da4
joaopmo <joao.oliveira@dcc.ufmg.br>
2022-11-13 20:11:16 -0300

    alterações

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -2,39 +2,41 @@ const { StatusCodes } = require("http-status-codes");
 const { authService } = require("../services");
 
 module.exports = {
-    signup: async (req, res) => {
-        try {
-            const signupResponse = await authService.signup(req.body);
+  signup: async (req, res) => {
+    try {
+      const signupResponse = await authService.signup(req.body);
 
-            return res.status(StatusCodes.OK).json(signupResponse);         
-        } catch (error) {
-            console.error(error);
-            return res
-                .status(
-                    error.name === "ValidationError"
-                    ? StatusCodes.UNPROCESSABLE_ENTITY
-                    : error.status || StatusCodes.INTERNAL_SERVER_ERROR
-                )
-                .json(error.message);
-        }
-    },
-
-    login: async (req, res) => {
-        try {
-            const userInfo = await authService.login(req.body);
-            const token = await authService.generateToken(userInfo)
-            return res.cookie('jwt', token, {
-                httpOnly: true,
-            }).json(userInfo);
-        } catch (error) {
-            console.error(error);
-            return res
-                .status(
-                    error.name === "ValidationError"
-                    ? StatusCodes.UNPROCESSABLE_ENTITY
-                    : error.status || StatusCodes.INTERNAL_SERVER_ERROR
-                )
-                .json(error.message);
-        }
+      return res.status(StatusCodes.OK).json(signupResponse);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(
+          error.name === "ValidationError"
+            ? StatusCodes.UNPROCESSABLE_ENTITY
+            : error.status || StatusCodes.INTERNAL_SERVER_ERROR
+        )
+        .json(error.message);
     }
+  },
+
+  login: async (req, res) => {
+    try {
+      const userInfo = await authService.login(req.body);
+      const token = await authService.generateToken(userInfo);
+      return res
+        .cookie("jwt", token, {
+          httpOnly: true,
+        })
+        .json(userInfo);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(
+          error.name === "ValidationError"
+            ? StatusCodes.UNPROCESSABLE_ENTITY
+            : error.status || StatusCodes.INTERNAL_SERVER_ERROR
+        )
+        .json(error.message);
+    }
+  },
 };
