8214c9f5a05f2eeb8c83e9bee4e7627cc5c01c30
Lucas August <nutkaugusto@gmail.com>
2022-11-13 12:53:59 -0300

    some change

---
+++ backend/src/db/index.js
@@ -0,0 +1,54 @@
+const { articleService, authService } = require("../services");
+
+module.exports = {
+    buildDB: async () => {
+        //Definindo os autores (Test)
+        authService.signup({
+            name: 'Kobayashi',
+            email: "teste@gmail.com",
+            password: "teste"
+        });
+
+
+        // Definindo os artigos (Test)
+        articleService.createArticle({
+            id: 0,
+            title: 'React Article',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum non diam eget posuere. Maecenas malesuada vitae turpis id malesuada. Donec ornare nibh a nunc facilisis, non porttitor felis convallis.',
+            content: "article content",
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+            ID_Author: 0,
+            updatedAt: '2022-04-25 21:48:56'
+        });
+
+        articleService.createArticle({
+            id: 1,
+            title: 'MySql Article',
+            description: 'Suspendisse varius odio eget posuere auctor. Aenean nec purus ipsum. Integer orci tellus, blandit et mauris id, efficitur ornare nunc. Nulla et ex vel odio aliquet tempus. Sed tortor metus, viverra ac enim sit amet, dignissim efficitur tellus.',
+            content: "article content",
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/MySQL_textlogo.svg/2560px-MySQL_textlogo.svg.png',
+            ID_Author: 0,
+            updatedAt: '2022-10-08 19:06:2'
+        });
+
+        articleService.createArticle({
+            id: 2,
+            title: 'Express Article',
+            description: 'Aliquam viverra sapien sed diam fermentum pellentesque. Nulla maximus enim a turpis sollicitudin lobortis. Praesent ut leo feugiat, tempor libero quis, lacinia libero. Duis nec dapibus nulla.',
+            content: "article content",
+            image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
+            ID_Author: 0,
+            updatedAt: '2022-06-18 11:50:28'
+        });
+
+        articleService.createArticle({
+            id: 3,
+            title: 'Node Article',
+            description: 'Cras tincidunt lectus quis sem gravida sodales. Donec malesuada erat ut tellus tempus, nec vestibulum odio vulputate.',
+            content: "article content",
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png',
+            ID_Author: 0,
+            updatedAt: '2022-09-02 12:08:43'
+        });
+    }
+}
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
         'root', 
         'root', {
         host: 'db',
-        dialect: 'mysql'
+        dialect: 'sqlite'
     });
 
 fs.readdirSync(__dirname)
