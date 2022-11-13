9862a86cad4739a9ce1883915ed0cdb0bb39b05c
iriasmatheus <55512225+iriasmatheus@users.noreply.github.com>
2022-11-13 15:17:48 -0300

    métodos das requisições

--- backend/src/routes/ArticleRoutes.js
+++ backend/src/routes/ArticleRoutes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const { articleController } = require("../controllers");
 
-router.get("/create-article", articleController.create);
-router.post("/get-article", articleController.getArticle);
-router.patch("/get-all-article", articleController.getAllArticles);
-router.delete("/get-articles-by-user", articleController.getArticleByUser);
-router.patch("/update-article", articleController.updateArticle);
+router.post("/create-article", articleController.create);
+router.get("/get-article", articleController.getArticle);
+router.get("/get-all-article", articleController.getAllArticles);
+router.get("/get-articles-by-user", articleController.getArticleByUser);
+router.put("/update-article", articleController.updateArticle);
 router.delete("/delete-article", articleController.autodeleteArticle);
 
 module.exports.article = router;
